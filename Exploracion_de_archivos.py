d047ed37077bfaaf44ecb6e1509236b053127608
munhof <munhofdev@gmail.com>
2023-11-05 11:08:39 -0300

    update Exploracion_de_archivos
    Cambios:
    >>código emprolijado en genera: variables mas descriptivas y mejor orden
    >>se guardan las imágenes generadas en carpeta: figuras-exploracion
    >>se cambio el nombre de la clase T-shirt/top a  T-shirt o top

--- Exploracion_de_archivos.py
+++ Exploracion_de_archivos.py
@@ -13,13 +13,14 @@ from inline_sql import sql, sql_val
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#configuracion necesaria de pyplot para ver las imagenes en escala de grises
+# configuracion necesaria de pyplot para ver las imagenes en escala de grises
 plt.rcParams['image.cmap'] = 'gray'
-
-#importo el data set
+# importo el data set
 dataset = pd.read_csv("./Dataset-original/fashion-mnist.csv")
 cod_prendas = pd.read_csv("./Dataset-original/cod-prendas.csv")
+# %%
 
+# analisamos el data set como conjunto
 print("Las columnas del data frame son")
 print(dataset.columns)
 print("Los valores unicos de label son")
@@ -28,38 +29,42 @@ print("Los valores unicos de alguna columnas pixel son")
 print(dataset.pixel88.unique())
 
 
-#si veo el tamaño del data set, tengo que es de 60000x785
-#es decir tenemos las 60000 prendas con sus imagenes de 24x24pixeles (784)
-#y el label identificatorio, que se encuentra en la primer columnas del atributo
+# si veo el tamaño del data set, tengo que es de 60000x785
+# es decir tenemos las 60000 prendas con sus imagenes de 24x24pixeles (784)
+# y el label identificatorio, que se encuentra en la primer columnas del atributo
 
-#el codigo de las prendas es 
-#0 T-shirt/top
-#1 Trouser
-#2 Pullover
-#3 Dress
-#4 Coat
-#5 Sandal
-#6 Shirt
-#7 Sneaker
-#8 Bag
-#9 Ankle boot
+# el codigo de las prendas es
+# 0 T-shirt/top
+# 1 Trouser
+# 2 Pullover
+# 3 Dress
+# 4 Coat
+# 5 Sandal
+# 6 Shirt
+# 7 Sneaker
+# 8 Bag
+# 9 Ankle boot
 
-#que estan cargados en el csv  cod-prendas
+# que estan cargados en el csv  cod-prendas
 
-#abro una imagen para ver como se comporta, para eso tomo la primera fila
+# abro una imagen para ver como se comporta, para eso tomo la primera fila
 
 primera_fila = dataset.loc[0].to_numpy()
 print("El tipo de la prenda es")
 print(primera_fila[0])
 print("Que es Pullover")
 
-#el primer elemento del array es el tipo, que es pullover
+# el primer elemento del array es el tipo, que es pullover
 primer_tipo = primera_fila[0]
 
-primera_imagen = primera_fila[1:].reshape([28,28])
+primera_imagen = primera_fila[1:].reshape([28, 28])
 plt.matshow(primera_imagen)
 
-#grafico uno de cada uno
+plt.savefig("./figuras-exploracion-de-datos/primera-prenda-dataset.png")
+plt.close()
+del primer_tipo, primera_imagen
+
+# grafico uno de cada uno
 fig, axs = plt.subplots(nrows=2, ncols=5, figsize=(10, 10),
                         subplot_kw={'xticks': [], 'yticks': []})
 
@@ -71,29 +76,72 @@ for ax, cod, prenda in zip(axs.flat, codigos, prendas):
     ax.set_title(str(prenda))
     dataset_prenda = dataset[dataset["label"] == cod]
     primera_fila = dataset_prenda.iloc[0].to_numpy()
-    primera_imagen = primera_fila[1:].reshape([28,28])
+    primera_imagen = primera_fila[1:].reshape([28, 28])
     ax.imshow(primera_imagen)
-    
-fig.show()
 
-#otra informacion interesante es saber cuantas de cada prenda hay cargadas
+plt.savefig("./figuras-exploracion-de-datos/primeras-prendas-de-cada-clase.png")
+plt.close()
+
+del ax, prenda, cod, axs, fig, primera_fila, primera_imagen
+
+# otra informacion interesante es saber cuantas de cada prenda hay cargadas
 
 for cod, prenda in zip(codigos, prendas):
     dataset_prenda = dataset[dataset["label"] == cod]
-    print("De " + prenda + " hay cargadas:" ,  dataset_prenda.shape[0])
+    print("De " + prenda + " hay cargadas:",  dataset_prenda.shape[0])
 
-#hay la misma cantidad cantidad de cada uno
+# hay la misma cantidad cantidad de cada uno
+del cod, prenda, codigos, prendas
+# %%
+# Aca analisamos como de comporta la informacion de cada pixel
+# tanto en conjunto como en cada grupo de prendas
 
+# creo un diccionario que guarda por separado las imagenes de cada tipo
+codigos = cod_prendas["Codigo"].array.to_numpy().reshape(10)
+prendas = cod_prendas["Tipo De Prenda"].array.to_numpy().reshape(10)
 
-
-#creo un diccionario que guarda por separado las imagenes de cada tipo
-    
 book_fotos = {}
 for cod in codigos:
-    book_fotos[cod] = dataset[dataset["label"] == cod].drop(columns = ["label"])
+    book_fotos[cod] = dataset[dataset["label"] == cod].drop(columns=["label"])
 
 
-#analizo la promedio de intensidad de un pixel en todo un tipo de prendas
+# analizo la promedio de intensidad de un pixel en todo un tipo de prendas
+fig, axs = plt.subplots(nrows=2, ncols=5, figsize=(10, 10),
+                        subplot_kw={'xticks': [], 'yticks': []})
+
+for ax, cod, prenda in zip(axs.flat, codigos, prendas):
+    ax.set_title(str(prenda))
+    dataset_prenda = book_fotos[cod]
+    promedio_intensidad_pixel = dataset_prenda.mean().to_numpy()
+    imagen_promedio_intensidad = promedio_intensidad_pixel.reshape([28, 28])
+    ax.imshow(imagen_promedio_intensidad)
+
+fig.suptitle("media de intensidad de" + prenda)
+fig.show()
+plt.savefig("./figuras-exploracion-de-datos/imagen-de-promedio-de-intensidad.png")
+plt.close()
+
+del ax, prenda, cod, axs, fig, promedio_intensidad_pixel, imagen_promedio_intensidad
+
+# el maximo
+fig, axs = plt.subplots(nrows=2, ncols=5, figsize=(10, 10),
+                        subplot_kw={'xticks': [], 'yticks': []})
+
+
+for ax, cod, prenda in zip(axs.flat, codigos, prendas):
+    ax.set_title(str(prenda))
+    dataset_prenda = book_fotos[cod]
+    maxima_intensidad_pixel = dataset_prenda.max().to_numpy()
+    imagen_maxima_intensidad = maxima_intensidad_pixel.reshape([28, 28])
+    ax.imshow(imagen_maxima_intensidad)
+
+fig.suptitle("maxima de intensidad de" + prenda)
+fig.show()
+plt.savefig("./figuras-exploracion-de-datos/imagen-de-maxima-de-intensidad.png")
+plt.close()
+del ax, prenda, cod, axs, fig, maxima_intensidad_pixel, imagen_maxima_intensidad
+
+# el minimo
 fig, axs = plt.subplots(nrows=2, ncols=5, figsize=(10, 10),
                         subplot_kw={'xticks': [], 'yticks': []})
 
@@ -103,119 +151,116 @@ prendas = cod_prendas["Tipo De Prenda"].array.to_numpy().reshape(10)
 
 for ax, cod, prenda in zip(axs.flat, codigos, prendas):
     ax.set_title(str(prenda))
-    dataset_prenda =book_fotos[cod]
-    primera_fila = dataset_prenda.mean().to_numpy()
-    primera_imagen = primera_fila.reshape([28,28])
-    ax.imshow(primera_imagen)
-    
-fig.show()
+    dataset_prenda = book_fotos[cod]
+    minima_intensidad_pixel = dataset_prenda.min().to_numpy()
+    imagen_minima_intensidad = minima_intensidad_pixel.reshape([28, 28])
+    ax.imshow(imagen_minima_intensidad)
 
-#el maximo
-fig, axs = plt.subplots(nrows=2, ncols=5, figsize=(10, 10),
+fig.suptitle("minima intensidad de" + prenda)
+fig.show()
+plt.savefig("./figuras-exploracion-de-datos/imagen-de-minima-de-intensidad.png")
+plt.close()
+del ax, prenda, cod, axs, fig, minima_intensidad_pixel, imagen_minima_intensidad
+
+# del caso del maximo de aparicion, entre todas las prendas puedo eliminar
+# los casos que nunca se utiliza a un pixel (si su valor maximo entre todas
+# las fotos es 0 entonces nunca se utiliza y no aporta informacion)
+
+fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(30, 30),
                         subplot_kw={'xticks': [], 'yticks': []})
 
-codigos = cod_prendas["Codigo"].array.to_numpy().reshape(10)
-prendas = cod_prendas["Tipo De Prenda"].array.to_numpy().reshape(10)
+maximos_prendas = dataset.max().array.to_numpy()[1:].reshape([28, 28])
+axs[0].matshow(maximos_prendas)
+axs[0].set_title("Maxima intensidad prendas")
 
+minimo_prendas = dataset.min().array.to_numpy()[1:].reshape([28, 28])
+axs[1].matshow(minimo_prendas)
+axs[1].set_title("Minima intensidad prendas")
 
-for ax, cod, prenda in zip(axs.flat, codigos, prendas):
-    ax.set_title(str(prenda))
-    dataset_prenda =book_fotos[cod]
-    primera_fila = dataset_prenda.max().to_numpy()
-    primera_imagen = primera_fila.reshape([28,28])
-    ax.imshow(primera_imagen)
-    
-fig.show()
+fig.suptitle("Maximo y minima intensidad en las prendas")
+plt.show()
+plt.savefig("./figuras-exploracion-de-datos/maximo-y-minimo-de-intensidad.png")
+plt.close()
+del axs, fig, maximos_prendas, minimo_prendas
 
-#el minimo
-fig, axs = plt.subplots(nrows=10, ncols=4, figsize=(10, 10),
+fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(30, 30),
                         subplot_kw={'xticks': [], 'yticks': []})
 
-codigos = cod_prendas["Codigo"].array.to_numpy().reshape(10)
-prendas = cod_prendas["Tipo De Prenda"].array.to_numpy().reshape(10)
+promedio_prendas = dataset.mean().array.to_numpy()[1:].reshape([28, 28])
+axs[0].matshow(promedio_prendas)
+axs[0].set_title("Media de intensidad prendas")
+
+media_prendas = dataset.median().array.to_numpy()[1:].reshape([28, 28])
+axs[1].matshow(media_prendas)
+axs[1].set_title("Mediana de intensidad prendas")
+
+desviacion_pix = dataset.std().array.to_numpy()[1:].reshape([28, 28])
+axs[2].matshow(desviacion_pix)
+axs[2].set_title("Desviacion estandar de intensidad prendas")
+
+fig.suptitle("Medidas resumen de intensidad en las prendas")
+plt.show()
+plt.savefig("./figuras-exploracion-de-datos/medidas-resumen-intensidad.png")
+plt.close()
+del axs, fig, promedio_prendas,media_prendas,desviacion_pix
+
+# grafico de la media,moda,std y maximo de cada prenda
 
 
-for ax, cod, prenda in zip(axs.flat, codigos, prendas):
-    ax.set_title(str(prenda))
-    dataset_prenda =book_fotos[cod]
-    primera_fila = dataset_prenda.min().to_numpy()
-    primera_imagen = primera_fila.reshape([28,28])
-    ax.imshow(primera_imagen)
-    
-fig.show()
-
-#del caso del maximo de aparicion, entre todas las prendas puedo eliminar 
-#los casos que nunca se utiliza a un pixel (si su valor maximo entre todas
-#las fotos es 0 entonces nunca se utiliza y no aporta informacion)
-
-
-maximos_prendas = dataset.max().array.to_numpy()[1:].reshape([28,28])
-plt.matshow(maximos_prendas)
-plt.title("Maxima intensidad prendas")
-
-minimo_prendas = dataset.min().array.to_numpy()[1:].reshape([28,28])
-plt.matshow(minimo_prendas)
-plt.title("minima intensidad prendas")
-
-promedio_prendas = dataset.mean().array.to_numpy()[1:].reshape([28,28])
-plt.matshow(promedio_prendas)
-plt.title("promedia intensidad prendas")
-
-
-media_prendas = dataset.median().array.to_numpy()[1:].reshape([28,28])
-plt.matshow(media_prendas)
-plt.title("media intensidad prendas")
-
-desviacion_pix = dataset.std().array.to_numpy()[1:].reshape([28,28])
-plt.matshow(desviacion_pix)
-plt.title("std intensidad prendas")
-
-#grafico de la media,moda,std y maximo de cada prenda
-
-fig, axs = plt.subplots(nrows=10, ncols=4, figsize=(10, 10),
-                        subplot_kw={'xticks': [], 'yticks': []})
-
-
-
-codigos = cod_prendas["Codigo"].array.to_numpy().reshape(10)
-prendas = cod_prendas["Tipo De Prenda"].array.to_numpy().reshape(10)
-grfico_title = ["media","moda","std","maximo"]
-
+grafico_title = ["media", "moda", "std", "maximo"]
 for i in range(10):
+
+    fig, axs = plt.subplots(nrows=1, ncols=4, figsize=(30, 30),
+                            subplot_kw={'xticks': [], 'yticks': []})
+    fig.suptitle("Medidas resumen de "+prendas[i])
     dataset_prenda = book_fotos[i]
     dataset_prenda.head()
     for j in range(4):
-        axs[i,j].set_title(str(prendas[i] + grfico_title[j]))
+        axs[j].set_title(grafico_title[j])
         if j == 0:
-            primera_fila = dataset_prenda.mean().to_numpy()
+            data_imagen = dataset_prenda.mean().to_numpy()
         elif j == 1:
-            primera_fila = dataset_prenda.median().to_numpy()
+            data_imagen = dataset_prenda.median().to_numpy()
         elif j == 2:
-            primera_fila = dataset_prenda.std().to_numpy()
+            data_imagen = dataset_prenda.std().to_numpy()
         else:
-            primera_fila = dataset_prenda.max().to_numpy()
-        primera_imagen = primera_fila.reshape([28,28])
-        axs[i,j].imshow(primera_imagen)
+            data_imagen = dataset_prenda.max().to_numpy()
+            
+        imagen = data_imagen.reshape([28, 28])
+        axs[j].imshow(imagen)
+    plt.show()
+    plt.savefig("./figuras-exploracion-de-datos/medidas-resumen-intensidad-por-prenda-"+ prendas[i] + ".png")
+plt.close()
+del i, j, axs, fig, data_imagen, imagen,grafico_title, dataset_prenda, codigos, prendas
 
-
+# %%
 
 # Analizo la variabilidad de los datos dentro de cada clase y las comparo entre si.
 
 desviaciones = np.ones(10)  # Placeholder
 
-for i in range (10):
-    desviacion_total_por_clase = dataset[dataset['label'] == i ].std().array.to_numpy()[1:].sum()
+for i in range(10):
+    desviacion_total_por_clase = book_fotos[i].std().array.to_numpy()[1:].sum()
     # Agarro una clase, calculo la desviacion de cada columna y luego sumo las columnas.
     desviaciones[i] = desviacion_total_por_clase
 
-desviacion_minima = np.min(desviaciones)
-desviacion_normalizada = desviaciones/desviacion_minima # Por esto use array, para poder normalizar facilmente.
+#normalizo las desviaciones
+desviacion_maxima = np.max(desviaciones)
+desviacion_normalizada = desviaciones/desviacion_maxima
 
-nombre_de_prendas = np.array(cod_prendas['Tipo De Prenda'])
-dataset = pd.DataFrame({'nombre_de_prendas': nombre_de_prendas, 'desviacion_normalizada': list(desviacion_normalizada)}, columns=['nombre_de_prendas', 'desviacion_normalizada'])
-dataset = dataset.sort_values('desviacion_normalizada')
-sns.scatterplot(data = dataset , y = 'nombre_de_prendas', x = 'desviacion_normalizada').set(title = 'Variabilidad normalizada de los datos de cada clase')
+nombre_de_prendas = cod_prendas['Tipo De Prenda'].to_numpy()
+
+desviaciones_por_prendas = pd.DataFrame({'nombre_de_prendas': nombre_de_prendas, 'desviacion_normalizada': list(
+    desviacion_normalizada)}, columns=['nombre_de_prendas', 'desviacion_normalizada'])
+
+desviaciones_por_prendas = desviaciones_por_prendas.sort_values('desviacion_normalizada')
+
+fig = plt.figure(figsize=(10,5))
+plt.grid()
+sns.scatterplot(data=desviaciones_por_prendas, x='nombre_de_prendas', y='desviacion_normalizada').set(
+    title='Variabilidad normalizada de los datos de cada clase')
 plt.show()
+plt.savefig("./figuras-exploracion-de-datos/variabilidad-de-prendas.png")
 plt.close()
-# Con esta metrica la variabilidad entre los vestidos es alrededor de un 33% mas grande que entre sneakers, 
+# Con esta metrica la variabilidad entre los vestidos es alrededor de un 33% mas grande que entre sneakers,
 # que son los que menos variabilidad tienen.
