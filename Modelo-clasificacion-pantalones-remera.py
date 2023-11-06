9b2732150d2fce3a0d8e40a19ba6dd04cb9d0ee9
munhof <munhofdev@gmail.com>
2023-11-05 21:07:34 -0300

    update Modelo clasificacion pantolenes remera
    cree el modelo en base a las distancias arquetipo

--- Modelo-clasificacion-pantalones-remera.py
+++ Modelo-clasificacion-pantalones-remera.py
@@ -50,8 +50,8 @@ def distancia_arquetipo(data_frame):
         arra_arquetipo_pantalon = pantalon_arquetipo.to_numpy()
         #hago que la intensidad este entre 0 y 1  dividiendo por 255 para
         #asi tener numero mas manejables
-        distancias_remera.append(np.linalg.norm(array_prenda/255-arra_arquetipo_remera/255,-2))
-        distancias_pantalon.append(np.linalg.norm(array_prenda/255-arra_arquetipo_pantalon/255,-2))
+        distancias_remera.append(np.linalg.norm(array_prenda/255-arra_arquetipo_remera/255))
+        distancias_pantalon.append(np.linalg.norm(array_prenda/255-arra_arquetipo_pantalon/255))
     return distancias_remera, distancias_pantalon
 
 distancia_remera,distancia_pantalon = distancia_arquetipo(data_pantalon_remera)
@@ -77,3 +77,66 @@ sns.scatterplot(data = data_pantalon_remera, y = "label", x = "distancia remera"
 plt.title("label vs distancia remera")
 plt.show()
 
+#vemos que hay dos grandes regiones donde tiene sentido realizar la clasificacion
+#con esta metrica, veamos que pasa con knn
+
+#importo la data para el modelo
+
+X_train, X_test, y_train, y_test = Limpieza_de_datos.data_train_modelo_pantalon_remera()
+
+#les calculo las distancias a test y train (modifico la funcion pra datos sin label)
+def distancia_arquetipo(data_frame):
+    distancias_remera = []
+    distancias_pantalon = []
+    n = np.shape(data_frame)[0]
+    for i in range(n):
+        data_prenda = data_frame.iloc[i]
+        array_prenda = data_prenda[:785].to_numpy()
+        arra_arquetipo_remera = remera_arquetipo.to_numpy()
+        arra_arquetipo_pantalon = pantalon_arquetipo.to_numpy()
+        #hago que la intensidad este entre 0 y 1  dividiendo por 255 para
+        #asi tener numero mas manejables
+        distancias_remera.append(np.linalg.norm(array_prenda/255-arra_arquetipo_remera/255))
+        distancias_pantalon.append(np.linalg.norm(array_prenda/255-arra_arquetipo_pantalon/255))
+    return distancias_remera, distancias_pantalon
+
+distancia_remera,distancia_pantalon = distancia_arquetipo(X_train)
+X_train["distancia remera"] = distancia_remera
+X_train["distancia pantalon"] = distancia_pantalon
+
+distancia_remera,distancia_pantalon = distancia_arquetipo(X_test)
+X_test["distancia remera"] = distancia_remera
+X_test["distancia pantalon"] = distancia_pantalon
+
+#ahora tomo los valores especificos con los cuales voy a trabajar
+x_train = X_train[["distancia remera","distancia pantalon"]]
+x_test = X_test[["distancia remera","distancia pantalon"]]
+
+#armo el modelo, arbitrariamente elijo 5
+knn_distancia = KNeighborsClassifier(n_neighbors=5) 
+
+#entreno el modelo
+knn_distancia.fit(x_train,y_train)
+
+
+data_test = x_test.copy()
+data_test["label"] = y_test.copy()
+
+data_test_modelo = x_test.copy()
+data_test_modelo["label"] = knn_distancia.predict(x_test)
+
+#reviso el score con los dato de test
+print(knn_distancia.score(x_test, y_test))
+
+fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10, 10))
+
+axs[0] = sns.scatterplot(data = data_test, x = "distancia remera", y = "distancia pantalon", 
+                         hue = "label", ax = axs[0] )
+axs[0].set_title("distribucion datos original")
+
+axs[1] = sns.scatterplot(data = data_test_modelo, x = "distancia remera", y = "distancia pantalon",
+                         hue = "label",  ax = axs[1] )
+axs[1].set_title("distribucion datos modelo")
+fig.suptitle("Modelo de clasficiacion de KNN por \n distancia arquetipos")
+
+#ahora comprobemos como se comporta el modelo variando el hyper parametro n_neighbors
